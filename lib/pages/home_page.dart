cd4c5e6be28234b7cde1876342187b9c90ccada8
Breno Lucas <brenolucas0106@gmail.com>
2022-04-05 00:25:07 -0300

    Adicionando list view na home page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -5,16 +5,282 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Text(
-          'Home page is ready!',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 30,
-          ),
+    return ListView(
+      scrollDirection: Axis.vertical,
+      padding: const EdgeInsets.only(top: 70),
+      children: [
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: const [
+                Text(
+                  'Boa noite',
+                  style: TextStyle(
+                    fontSize: 25,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                Icon(
+                  Icons.notifications_none_outlined,
+                ),
+                Icon(
+                  Icons.schedule_outlined,
+                ),
+                Icon(
+                  Icons.settings_outlined,
+                ),
+              ],
+            ),
+            Container(
+              child: const Text(
+                'Tocadas Recentemente',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              margin: const EdgeInsets.fromLTRB(10, 0, 0, 5),
+            ),
+            SizedBox(
+              height: 200,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+                children: [
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: const Text(
+                'Seus artistas favoritos',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              margin: const EdgeInsets.fromLTRB(10, 0, 0, 5),
+            ),
+            SizedBox(
+              height: 200,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+                children: [
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: const Text(
+                'Suas playlists',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              margin: const EdgeInsets.fromLTRB(10, 0, 0, 5),
+            ),
+            SizedBox(
+              height: 200,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+                children: [
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Container(
+              child: const Text(
+                'Artistas populares',
+                style: TextStyle(
+                  fontSize: 25,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              margin: const EdgeInsets.fromLTRB(10, 0, 0, 5),
+            ),
+            SizedBox(
+              height: 200,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+                children: [
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                  Card(
+                    color: Colors.blue,
+                    child: Container(
+                      width: 200,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
         ),
-      ),
+      ],
     );
   }
 }
